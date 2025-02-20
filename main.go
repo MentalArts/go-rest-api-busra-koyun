fc8c6fbb58cc9f84306f1e29bdde9f14ac8749e4
Büşra Koyun <busrakoyun99@gmail.com>
2025-02-20 17:47:49 +0300

    feat(api): add examples for params and payloads

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,8 @@ type Response struct {
 func main() {
 	router := gin.Default()
 	router.GET("/ping", handlePing)
+	router.GET("/hello", handleHello)
+	router.GET("/helloWithPayload", handleHelloWithPayload)
 	router.Run(":8080")
 }
 
@@ -21,13 +24,50 @@ func handlePing(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
+func handleHello(c *gin.Context) {
+	name := c.Query("name")
+
+	var msg string
+	if name != "" {
+		msg = fmt.Sprintf("Welcome, %s", name)
+	} else {
+		msg = "Welcome, user"
+	}
+
+	c.String(http.StatusOK, msg)
+}
+
+type DTO struct {
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+}
+
+func handleHelloWithPayload(c *gin.Context) {
+	//binding (get payload from request)
+	var dto DTO
+
+	err := c.BindJSON(&dto)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+	//validation (validate the payload)
+	if dto.Name == "" || dto.Surname == "" {
+		c.String(http.StatusBadRequest, "EMPTY NAME OR SURNAME FIELD")
+		return
+	}
+
+	msg := fmt.Sprintf("Hello, %s %s", dto.Name, dto.Surname)
+	c.String(http.StatusOK, msg)
+}
+
 /*
+//Vanilla Implemantation
 import (
 	"encoding/json"
 	"log"
 	"net/http"
 )
-//Vanilla Implemantation
 func main() {
 	http.HandleFunc("GET /ping", handlePing)
 	log.Println("Server listening...")
